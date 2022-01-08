2f4abe74485c61876ef6733638eec4c1a2d80e72
Manasi Paradkar <paradkar.manasi@gmail.com>
2022-01-08 08:35:20 +0000

    Update Appt model and add urls.py

--- appt/views.py
+++ appt/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from .models import Appt
 
-# Create your views here.
+
+class HomeTemplateView(TemplateView):
+    template_name = 'index.html'
